03d5edec11191da95ecdec11d3f32669206d149c
Justin Platz <jplatz@umich.edu>
2015-06-26 17:00:15 -0700

    broken history of my keys

--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 platform :ios, '8.0'
 target 'QuickSwift' do
 source 'https://github.com/CocoaPods/Specs.git'
-pod 'PubNub', :git => 'https://github.com/pubnub/objective-c.git', :branch => '4.0b2'
+pod 'PubNub', :git => 'https://github.com/pubnub/objective-c.git', :branch => '4.0b3'
 end
 target 'QuickSwiftTests' do
 end
--- QuickSwift/AppDelegate.swift
+++ QuickSwift/AppDelegate.swift
@@ -15,22 +15,75 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PNObjectEventListener {
     
     var client:PubNub?
 
-
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         let config = PNConfiguration(
             publishKey: "pub-c-f83b8b34-5dbc-4502-ac34-5073f2382d96",
             subscribeKey: "sub-c-34be47b2-f776-11e4-b559-0619f8945a4f")
+//        let config = PNConfiguration(
+//            publishKey: "demo-36", subscribeKey: "demo-36"
+//        )
         
         client = PubNub.clientWithConfiguration(config)
         
         client?.subscribeToChannels(["demo"], withPresence: false)
         
+        client?.subscribeToPresenceChannels(["demo"])
+        
+//        NSNumber *startDate = @((unsigned long long)([[NSDate dateWithTimeIntervalSinceNow:-(60*60)] timeIntervalSince1970]*10000000));
+//        NSNumber *endDate = @((unsigned long long)([[NSDate date] timeIntervalSince1970]*10000000));
+    
+        let startInterval:NSNumber = NSDate(timeIntervalSinceReferenceDate: -(60*60)).timeIntervalSince1970*10000000
+        let endInterval:NSNumber = NSDate().timeIntervalSince1970*10000000
+        
+        var first:NSNumber = NSDate(timeIntervalSinceNow: -600).timeIntervalSince1970*10000000
+        var second:NSNumber =  NSDate(timeIntervalSinceNow: -30).timeIntervalSince1970*10000000
+        
+//        var one: NSNumber = 14350904008290302
+//        var two: NSNumber = 14350906104420848
+        var one: NSNumber = NSNumber(longLong: 14350904008290302)
+        var two: NSNumber = NSNumber(longLong: 14350906104420848)
+        
+        client?.historyForChannel("demo", start: one, end: two, includeTimeToken: true, withCompletion: { (result, status) -> Void in
+            
+            println(result!)
+        })
+
+//        client?.historyForChannel("demo", start: one, end: two, includeTimeToken: true, withCompletion: { (PNHistoryResult result, PNErrorStatus status) -> Void in
+//            
+//            // Check whether request successfully completed or not.
+//            //if (!status.isError) {
+//                
+//                // Handle downloaded history using:
+//                //   result.data.start - oldest message time stamp in response
+//                //   result.data.end - newest message time stamp in response
+//                //   result.data.messages - list of dictionaries. Each entry will include two keys:
+//                //                          "message" - for body and "timetoken" for date when message has
+//                //                          been sent.
+//                println("******historyForChannel*****")
+//                println("First is ")
+//                println(first)
+//                println("Second is ")
+//                println(second)
+//                println(result.data.start)
+//                println(result.data.end)
+//                println(result.data.messages)
+//
+//            //}
+//                 //Request processing failed.
+//            //else {
+//                
+//                 //Handle message history download error. Check 'category' property to find out possible
+//                 //issue because of which request did fail.
+//            
+//                 //Request can be resend using: [status retry];
+//            //}
+//        })
+        
         client?.publish("Swift + PubNub!", toChannel: "demo", compressed: false, withCompletion: nil)
         
-        client?.addListeners([self])
+        client?.addListener(self)
         
         return true
     }
@@ -57,17 +110,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PNObjectEventListener {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func client(client: PubNub!, didReceiveMessage message: PNResult!, withStatus status: PNStatus!) {
-        println(message)
+
+    func client(client: PubNub!, didReceiveMessage message: PNMessageResult!, withStatus status: PNErrorStatus!) {
+        println("******didReceiveMessage*****")
+        println(message.data)
     }
     
-
-
-    
-    
-
-    
-
+    func client(client: PubNub!, didReceivePresenceEvent event: PNPresenceEventResult!) {
+        println("******didReceivePresenceEvent*****")
+        println(event.data)
+    }
 
 }
 
